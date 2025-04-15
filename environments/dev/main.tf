c20389f01731d5f44fd6b1382391662484c14417
Robert Smith <ruckom@hotmail.com>
2025-04-15 11:06:38 +0100

    Add environment files

---
+++ environments/dev/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+module "vnet" {
+  source              = "../../modules/vnet"
+  vnet_name           = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+  subnet_name         = var.subnet_name
+  subnet_prefixes     = var.subnet_prefixes
+}
